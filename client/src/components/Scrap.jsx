14ae605e548eca0525cc00dc9acf564c6ffaee47
az-m <alisonzmills@gmail.com>
2025-06-13 10:52:44 +0100

    feat: pull full list of data

---
+++ client/src/components/Scrap.jsx
@@ -0,0 +1,7 @@
+export default function Scrap() {
+  return (
+    <>
+      <h2>scrap</h2>
+    </>
+  );
+}
---
+++ client/src/pages/Page1.jsx
@@ -0,0 +1,9 @@
+import FullList from "../components/FullList";
+
+export default function Page1() {
+  return (
+    <>
+      <FullList />
+    </>
+  );
+}
